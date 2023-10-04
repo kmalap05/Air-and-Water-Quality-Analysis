10f37431d3bf860404f736787644d44ab2259457
kmalap05 <83391228+kmalap05@users.noreply.github.com>
2023-10-04 22:56:09 +0530

    api properly fetching data

--- api/main.js
+++ api/main.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
 const app = express();
 
 // Variables
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Api is working!');
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/fields-data/:field', async (req, res) => {
+  const response = await axios({
+    method: 'GET',
+    url: `https://api.thingspeak.com/channels/2279831/fields/${req.params.field}.json`
+  });
+
+  res.status(200).json(response.data.feeds);
 });
 
 app.listen(PORT);
